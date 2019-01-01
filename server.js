0f3969fdbbf727832aaa0497402f69d21ad002a1
jarvis394 <vlad.ekushev@mail.ru>
2019-01-01 14:37:04 +0300

    Add JSDOC temlpate

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get('/test', async (req, res) => {
 });
 
 app.get('/docs', async (req, res) => {
-  res.sendFile(__dirname + '/out/index.html'); 
+  res.sendFile(__dirname + '/docs/index.html'); 
 });
 
 var listener = app.listen(4000, function() {
